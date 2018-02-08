06d79c15a2394c73f07f919c2bf30dcec9d9c07e
lulu.li29 <s987337613@gmail.com>
2018-02-08 23:14:45 +0800

    add Fifteen love test

--- TennisGames/TennisGames/UnitTest1.cs
+++ TennisGames/TennisGames/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace TennisGamesTest
 {
@@ -10,17 +9,40 @@ namespace TennisGamesTest
         public void Love_All()
         {
             var tennisGames = new TennisGames();
-            var score = tennisGames.Score();
-            Assert.AreEqual("Love_All",score);
+            ScoreShouldBe("Love_All", tennisGames.Score());
         }
-        
+
+        [Test]
+        public void Fifteen_Love()
+        {
+            var tennisGames = new TennisGames();
+            tennisGames.FirstPlayerScore();
+            ScoreShouldBe("Fifteen_Love", tennisGames.Score());
+        }
+
+        private void ScoreShouldBe(string expected, string actual)
+        {
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class TennisGames
     {
         public string Score()
         {
+            if (FirstPlayerScoreTimes == 1)
+            {
+                return "Fifteen_Love";
+            }
+
             return "Love_All";
         }
+
+        public void FirstPlayerScore()
+        {
+            FirstPlayerScoreTimes++;
+        }
+
+        public int FirstPlayerScoreTimes { get; set; }
     }
 }
